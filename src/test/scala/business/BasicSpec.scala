ee0d9146d37f0c173e6066451f4531c936aab256
koooyooo <jab.cross.hook@gmail.com>
2019-07-15 04:28:01 +0900

    fix Basic Spec

--- src/test/scala/business/BasicSpec.scala
+++ src/test/scala/business/BasicSpec.scala
@@ -13,8 +13,9 @@ class BasicSpec extends WordSpec with Matchers {
 
     def changePass(account: UserAccount, oldPass: String, newPass: String): Try[UserAccount] =
       oldPass match {
+        // copyを利用することで一部属性のみ変更できる
         case account.pass => Success(account.copy(pass = newPass))
-        case _ => Failure(new Exception("password not match"))
+        case _ => Failure(new Exception("password does not match"))
       }
 
     def logOut(account: UserAccount): Try[UserAccount] = Try {
@@ -25,14 +26,56 @@ class BasicSpec extends WordSpec with Matchers {
   object UserAccountService extends UserAccountModule
 
   "UserAccount" should {
-    "" in {
-      val account = UserAccountService.login("my-id", "my-pass")
-      assert(account.isSuccess)
-      account.foreach{ u =>
-        assert(u.id == "my-id")
-        assert(u.pass == "my-pass")
+
+    "パイプラインとして扱うことができる" in {
+      val result =
+        for {
+          a <- UserAccountService.login("my-id", "my-pass")
+          p <- UserAccountService.changePass(a, "my-pass", "my-pass-new")
+          o <- UserAccountService.logOut(p)
+        } yield (a, p, o)
+
+      assert(result.isSuccess)
+
+      result match {
+        case Success(a) => {
+          assert(a._1.id == "my-id")
+          assert(a._1.pass == "my-pass")
+          assert(a._1.login == true)
+
+          assert(a._2.id == "my-id")
+          assert(a._2.pass == "my-pass-new")
+          assert(a._2.login == true)
+
+          assert(a._3.id == "my-id")
+          assert(a._3.pass == "my-pass-new")
+          assert(a._3.login == false)
+        }
+        case Failure(e) => fail()
       }
     }
+
+    "パイプライン失敗時には処理キャンセルが走る" in {
+      val result =
+        for {
+          a <- UserAccountService.login("my-id", "my-pass")
+          // パスワードの照合に失敗
+          p <- UserAccountService.changePass(a, "wrong-pass", "my-pass-new")
+          o <- UserAccountService.logOut(p)
+        } yield (a, p, o)
+
+      assert(result.isFailure)
+
+      result match {
+        case Success(a) => fail()
+        case Failure(e) => {
+          assert(e.isInstanceOf[Exception])
+          assert(e.getMessage == "password does not match")
+        }
+      }
+    }
+
   }
+
 
 }
